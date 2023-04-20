a88ab5ba52a76b121ce7595ad72996d1835598b9
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-21 00:24:55 +0300

    chore: rename file

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * insertion_sort_list - implements the insertion sort in a linked list
+ *
+ * @list: the list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+}
