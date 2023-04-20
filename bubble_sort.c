456e4ae738a6cdecdfba35690f77f24959203b5d
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-21 00:08:23 +0300

    feat: optimise best case to O(n) for bubble sort

--- bubble_sort.c
+++ bubble_sort.c
@@ -1,4 +1,4 @@
-#include <stdbool.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "sort.h"
 
@@ -10,24 +10,25 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	int tmp;
-	bool swapped;
+	size_t i, j, loops;
+	int tmp, swapped;
 
-	while(true)
+	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
-		for (i = 0; i < size - 1; i++)
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				tmp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tmp;
-				swapped = true;
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swapped = 1;
 				print_array(array, size);
 			}
 		}
+
 		if (!swapped)
 			break;
 	}
