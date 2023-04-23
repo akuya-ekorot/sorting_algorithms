c64ff94d52bdd981bacf9df207a25e3662d473c2
Akuya Ekorot <akuyaekorot@gmail.com>
2023-04-23 23:28:59 +0300

    fix: unused variable

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, loops;
+	size_t i, j;
 	int tmp, swapped;
 
 	if (size < 2)
--- 0-main.c
+++ 0-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int array[] = {1, 2, 3, 4, 5};
+	int array[] = {9, 1, 0, 4, 5};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
